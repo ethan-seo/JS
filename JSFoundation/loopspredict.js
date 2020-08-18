8174b5d9716ceb25e97b768e96fab1b7457a1356
Ethan Seo <sung.min.seo.428@gmail.com>
2020-08-17 23:13:37 -0700

    Loopspredict update and predicitions

--- JSFoundation/loopspredict.js
+++ JSFoundation/loopspredict.js
@@ -1,5 +1,22 @@
-function sayHello(name) {
-    console.log('Hello ' + name);
+for(var num = 0; num < 15; num++){
+    console.log(num);
 }
+//Prediction: number log from 0 to 14
 
-sayHello('Ethan');
+for(var i = 1; i < 10; i+=2){
+    if(i % 3 == 0){
+        console.log(i);
+    }
+}
+//Prediction: number log 3 and 9
+
+for(var j = 1; j <= 15; j++){
+    if(j % 2 == 0){
+        j+=2;
+    }
+    else if(j % 3 == 0){
+        j++;
+    }
+    console.log(j);
+}
+//Prediction: number log 1,4,5,8,10,11,14,16
